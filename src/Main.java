003b6f1515211dac9a91d50afb4d0e73dfd9ec5c
r.solikhov <r.solikhov@g.nsu.ru>
2025-04-22 15:38:44 +0300

    added: factory, factory_product

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
-    public static void main(String[] args) {
-        
+    public static void printSomething() {
+        System.out.println("thread check");
+    }
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread1 = new Thread();
+        Thread thread2 = new Thread();
+        thread2.start();
+        thread1.start();
+        thread1.join();
+        thread2.join();
     }
 }
