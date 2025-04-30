f91c2e5860da280c51f5c28ef0dfd9806c9af126
r.solikhov <r.solikhov@g.nsu.ru>
2025-04-30 17:10:18 +0300

    added: threads, thread pool

---
+++ src/threadpool/TaskExecuter.java
@@ -0,0 +1,23 @@
+package threadpool;
+
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class TaskExecuter  extends Thread {
+    LinkedBlockingQueue<Task> tasks;
+    public TaskExecuter(LinkedBlockingQueue<Task> tasks) {
+        super();
+        this.tasks = tasks;
+    }
+    @Override
+    public void run() {
+        while (isAlive()) {
+            try {
+                Task task = tasks.take();
+                System.out.println(threadId());
+                task.execute();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
